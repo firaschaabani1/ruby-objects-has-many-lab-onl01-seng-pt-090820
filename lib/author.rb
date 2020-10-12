4a4011a9ead0e6d578ca01d76e935032b4cade9c
Firas Chaabani <firaschaabani1@gmail.com>
2020-10-12 02:59:19 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -8,3 +8,14 @@ class Author
     @name = name
     @posts = Post.all
   end
+  
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+end
