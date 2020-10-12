3cfc1b2603e1c0317d0768bb299ef3a5a15ce3da
Firas Chaabani <firaschaabani1@gmail.com>
2020-10-12 03:02:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,4 +8,15 @@ class Song
     @artist = artist
     @@all << self
   end
+def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def self.all
+    @@all
+  end
 end
